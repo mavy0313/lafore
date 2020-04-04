40c4ccf1e7f7bc72bca5a8af824c3eee8a91a97b
mavy0313 <vypoff@gmail.com>
2020-04-05 03:41:46 +0400

    Stack and queue based on link List

---
+++ src/main/java/chapter5/stack/Link.java
@@ -0,0 +1,13 @@
+package chapter5.stack;
+
+public class Link {
+  public long dData; // Данные
+  public Link next; // Следующий элемент в списке
+// -------------------------------------------------------------
+  public Link(long dd) // Конструктор
+    { dData = dd; }
+// -------------------------------------------------------------
+    public void displayLink() // Вывод содержимого элемента
+    { System.out.print(dData + " "); }
+} // Конце класса Link
+
---
+++ src/main/java/chapter5/stack/LinkStack.java
@@ -0,0 +1,32 @@
+package chapter5.stack;
+
+public class LinkStack {
+
+  private LinkList theList;
+  //--------------------------------------------------------------
+  public LinkStack() // Конструктор
+  {
+    theList = new LinkList();
+  }
+  //--------------------------------------------------------------
+  public void push(long j) // Размещение элемента на вершине стека
+  {
+    theList.insertFirst(j);
+  }
+
+  public long pop() // Извлечение элемента с вершины стека
+  {
+    return theList.deleteFirst();
+  }
+  //--------------------------------------------------------------
+  public boolean isEmpty() // true, если стек пуст
+  {
+    return ( theList.isEmpty() );
+  }
+  //--------------------------------------------------------------
+  public void displayStack()
+  {
+    System.out.print("Stack (top-->bottom): ");
+    theList.displayList();
+  }
+}
