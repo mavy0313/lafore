08f16ad1af2137f4a2984c7de993cb69f0a45635
mavy0313 <vypoff@gmail.com>
2020-04-14 00:32:13 +0400

    Doubly linked list

---
+++ src/main/java/chapter5/doublylinked/Link.java
@@ -0,0 +1,13 @@
+package chapter5.doublylinked;
+
+class Link {
+  public long dData; // Данные
+  public Link next; // Следующий элемент в списке
+  public Link previous; // Предыдущий элемент в списке
+  // -------------------------------------------------------------
+  public Link(long d) // Конструктор
+  { dData = d; }
+  // -------------------------------------------------------------
+  public void displayLink() // Вывод содержимого элемента
+  { System.out.print(dData + " "); }
+}
