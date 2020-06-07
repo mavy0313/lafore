0077686b9b40d89b095f45a635b7439a3c4b7d71
max <vypoff@gmail.com>
2020-06-07 17:16:59 +0400

    Tree insert and symmetric traverse

--- src/main/java/chapter8/Tree.java
+++ src/main/java/chapter8/Tree.java
@@ -20,9 +20,51 @@ class Tree {
 
   public void insert(int id, double dd)
   {
+    Node newNode = new Node(); // Создание нового узла
+    newNode.iData = id; // Вставка данных
+    newNode.fData = dd;
+    if(root==null) // Корневой узел не существует
+      root = newNode;
+    else // Корневой узел занят
+    {
+      Node current = root; // Начать с корневого узла
+      Node parent;
+      while(true) // (Внутренний выход из цикла)
+      {
+        parent = current;
+        if(id < current.iData) // Двигаться налево?
+        {
+          current = current.leftChild;
+          if(current == null) // Если достигнут конец цепочки
+          { // вставить слева
+            parent.leftChild = newNode;
+            return;
+          }
+        }
+        else // Или направо?
+        {
+          current = current.rightChild;
+          if(current == null) // Если достигнут конец цепочки,
+          { // вставить справа
+            parent.rightChild = newNode;
+            return;
+          }
+        }
+      }
+    }
   }
 
   public void delete(int id)
   {
+  }
+
+  private void inOrder(Node localRoot)
+  {
+    if(localRoot != null)
+    {
+      inOrder(localRoot.leftChild);
+      System.out.print(localRoot.iData + " ");
+      inOrder(localRoot.rightChild);
+    }
   }
 }
