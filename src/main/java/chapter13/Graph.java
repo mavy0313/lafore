72c90d5dd994dacf4bd04aef10b39efc1584e357
mavy0313 <vypoff@gmail.com>
2020-08-25 01:04:15 +0300

    DFS

--- src/main/java/chapter13/Graph.java
+++ src/main/java/chapter13/Graph.java
@@ -5,6 +5,7 @@ class Graph {
   private Vertex vertexList[]; // Массив вершин
   private int adjMat[][]; // Матрица смежности
   private int nVerts; // Текущее количество вершин
+  private StackX theStack;
 // -------------------------------------------------------------
 public Graph() // Конструктор
   {
@@ -12,9 +13,12 @@ public Graph() // Конструктор
 // Матрица смежности
     adjMat = new int[MAX_VERTS][MAX_VERTS];
     nVerts = 0;
-    for(int j=0; j<MAX_VERTS; j++) // Матрица смежности
-      for(int k=0; k<MAX_VERTS; k++) // заполняется нулями
+    for(int j=0; j<MAX_VERTS; j++) { // Матрица смежности
+      for (int k = 0; k < MAX_VERTS; k++) { // заполняется нулями
         adjMat[j][k] = 0;
+      }
+    }
+    theStack = new StackX();
   }
 // -------------------------------------------------------------
   public void addVertex(char lab) // В аргументе передается метка
@@ -31,5 +35,37 @@ public Graph() // Конструктор
   public void displayVertex(int v)
   {
     System.out.print(vertexList[v].label);
+  }
+
+  public void dfs() // Обход в глубину
+  { // Алгоритм начинает с вершины 0
+    vertexList[0].wasVisited = true; // Пометка
+    displayVertex(0); // Вывод
+    theStack.push(0); // Занесение в стек
+    while( !theStack.isEmpty() ) // Пока стек не опустеет
+    {
+// Получение непосещенной вершины, смежной к текущей
+      int v = getAdjUnvisitedVertex( theStack.peek() );
+      if(v == -1) // Если такой вершины нет,
+        theStack.pop(); // элемент извлекается из стека
+      else // Если вершина найдена
+      {
+        vertexList[v].wasVisited = true; // Пометка
+        displayVertex(v); // Вывод
+        theStack.push(v); // Занесение в стек
+      }
+    }
+// Стек пуст, работа закончена
+    for(int j=0; j<nVerts; j++) // Сброс флагов
+      vertexList[j].wasVisited = false;
+  }
+  // ------------------------------------------------------------
+// Метод возвращает непосещенную вершину, смежную по отношению к v
+  public int getAdjUnvisitedVertex(int v)
+  {
+    for(int j=0; j<nVerts; j++)
+      if(adjMat[v][j]==1 && vertexList[j].wasVisited==false)
+        return j; // Возвращает первую найденную вершину
+    return -1; // Таких вершин нет
   }
 }
