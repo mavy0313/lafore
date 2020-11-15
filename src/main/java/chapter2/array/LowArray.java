95c981a78456ca7175a498dae8497043764b1d28
Max <vypoff@gmail.com>
2020-11-15 20:13:20 +0300

    chapter2 arrays

---
+++ src/main/java/chapter2/array/LowArray.java
@@ -0,0 +1,14 @@
+package chapter2.array;
+
+public class LowArray {
+  private long[] a; // Ссылка на массив a
+  //--------------------------------------------------------------
+  public LowArray(int size) // Конструктор
+  { a = new long[size]; } // Создание массива
+  //--------------------------------------------------------------
+  public void setElem(int index, long value) // Запись элемента
+  { a[index] = value; }
+  //--------------------------------------------------------------
+  public long getElem(int index) // Чтение элемента
+  { return a[index]; }
+}
